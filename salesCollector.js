923ed733ec39296a6d59b8ab53bcf3f39d37886d
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-08 23:06:12 -0400

    Converts request for ah data to function, removes test console.logs, adds url for alterac mountions server.

--- salesCollector.js
+++ salesCollector.js
@@ -14,57 +14,69 @@ data, ahData: json data of ah records
 // url to Blizzard API for Thrall server ah information 
 var url = `https://us.api.battle.net/wow/auction/data/thrall?locale=en_US&apikey=${process.env.API_KEY}`;
 
-// Object to hold data related to ah download data
-var ahDownloadData = {};
+// url to Blizzard API for Alterac Mountains server ah information 
+// var url = `https://us.api.battle.net/wow/auction/data/alterac%20mountains?locale=en_US&apikey=${process.env.API_KEY}`;
 
-request(
-  {
-    url: url,
-    json: true
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log("firstbody:", body);
 
-      // check body.lastModified with previous request
-      // and only pull data if the content has been
-      // modified.
-      // if (body.lastModified !== XXXXXX.timeStamp)
+retrieveAHData = (serverUrl) => {
 
-      ahDownloadData.url = body.files[0].url;
-      ahDownloadData.lastModified = body.files[0].lastModified;
+  // Object to hold data related to ah download data
+  var ahDownloadData = {};
 
-      var downloadStartTime = Date.now();
-
-      request(
-        {
-          url: ahDownloadData.url,
-          json: true
-        }, function (err, res, data) {
-          if (!err & res.statusCode === 200) {
-            
-            var downloadEndTime = Date.now();
-            var downloadElapsedTimeMilliseconds = downloadEndTime - downloadStartTime;
-            var filename = `${__dirname}/ahData/AHData_${Date.now()}.json`;
-
-            ahDownloadData.downloadElapsedTimeMilliseconds = downloadElapsedTimeMilliseconds;
-            ahDownloadData.ahData = data;
-            ahDownloadData.filename = filename;
-
-            var currentAHData = JSON.stringify(ahDownloadData);
-            
-            console.log("passed loading");
-            fs.writeFile(filename, currentAHData, (err) => {
-              if (err) {
-                console.log(`err: ${err}`);
-              } else {
-                console.log(`File ${filename} saved`);
-                console.log("Full AH Object: ", ahDownloadData);
-                console.log("Stringified Object: ", currentAHData);
-              }
-            });
+  request(
+    {
+      url: url,
+      json: true
+    }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log("firstbody:", body);
+  
+        // check body.lastModified with previous request
+        // and only pull data if the content has been
+        // modified.
+        // if (body.lastModified !== XXXXXX.timeStamp)
+  
+        ahDownloadData.url = body.files[0].url;
+        ahDownloadData.lastModified = body.files[0].lastModified;
+  
+        var downloadStartTime = Date.now();
+  
+        request(
+          {
+            url: ahDownloadData.url,
+            json: true
+          }, function (err, res, data) {
+            if (!err & res.statusCode === 200) {
+              
+              var downloadEndTime = Date.now();
+              var downloadElapsedTimeMilliseconds = downloadEndTime - downloadStartTime;
+              var filename = `${__dirname}/ahData/AHData_${Date.now()}.json`;
+  
+              ahDownloadData.downloadElapsedTimeMilliseconds = downloadElapsedTimeMilliseconds;
+              ahDownloadData.ahData = data;
+              ahDownloadData.filename = filename;
+  
+              var currentAHData = JSON.stringify(ahDownloadData);
+              
+              console.log("passed loading");
+              fs.writeFile(filename, currentAHData, (err) => {
+                if (err) {
+                  console.log(`err: ${err}`);
+                } else {
+                  console.log(`File ${filename} saved`);
+                }
+              });
+            }
           }
-        }
-      );
+        );
+      }
     }
-  }
-);
+  );
+};
+
+retrieveAHData(url);
+
+// setInterval(() => {
+//   console.log("time running");
+// }, 1500);
+

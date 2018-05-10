c282206073d7432d2b99e1f6164ba3bb4055c703
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-10 15:01:44 -0400

    Adds test code for retrieving data from blizzard api and inserting into database.

--- db/seed/seedDB.js
+++ db/seed/seedDB.js
@@ -3,5 +3,10 @@ const client = require("../client");
 
 // use this function call to drop current tables and recreate
 // this will delete any data currently stored in the db
-// const seed = require("./createTables");
-// seed.createTables(client);
+// const seedDatabase = require("./createTables");
+// seedDatabase.createTables(client);
+
+
+// used to test retrieving api data and inserting to db
+// const seedRecipeTable = require("./insertRecipe");
+// seedRecipeTable.insertRecipeToDB(client);

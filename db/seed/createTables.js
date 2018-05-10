48871cfe3922110539bedde0d791047b3c930825
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-09 21:05:55 -0400

    Adds database connection file, create/drop tables queries, and table initialization seed function.

---
+++ db/seed/createTables.js
@@ -0,0 +1,55 @@
+module.exports = {
+  
+  dropTables: function() {
+    
+
+  },
+
+  createTables: function(client) {
+    
+    client.connect((err) => {
+      if(!err) {
+        client.query(`${this.queries.dropTables} 
+                      ${this.queries.createTables}`, 
+                      (err, result) => {
+                        if(err) {
+                          console.log(err);
+                        } else {
+                          // console.log(result);
+                        }
+                        client.end();
+                      });
+      } else {
+        console.log(err);
+      }
+    });
+
+  },
+
+  queries: {
+    dropTables: `DROP TABLE IF EXISTS ahRecord;
+                DROP TABLE IF EXISTS ahDump;`,
+    createTables: `CREATE TABLE ahDump (
+                  id SERIAL primary key,
+                  url text,
+                  lastModified int,
+                  downloadDuration int
+                  );
+                  CREATE TABLE ahRecord (
+                  id SERIAL primary key,
+                  groupId int references ahDump(id),
+                  auctionId int,
+                  item int,
+                  owner text,
+                  ownerRealm text,
+                  bid int,
+                  buyout int,
+                  quantity int,
+                  timeLeft int,
+                  rand int,
+                  seed int,
+                  context int
+                  );
+                  `
+  }
+}

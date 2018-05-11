2ee0c2635f161b224e1f80e38b6363ee42edd4d4
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-11 15:12:53 -0400

    Adds test code trying to insert ahData records <-- fails to insert after 1000 records are inserted with no error thrown

--- db/seed/seedDB.js
+++ db/seed/seedDB.js
@@ -1,5 +1,5 @@
 const client = require("../client");
-
+require('dotenv').config();
 
 // use this function call to drop current tables and recreate
 // this will delete any data currently stored in the db
@@ -14,22 +14,44 @@ const client = require("../client");
 // test to seed database with ah data
 const ahData = require("../../ahData/AHData_1525812207294.json");
 var auctionCount = ahData.auctions.length;
-const { Pool } = require("pg");
-const pool = new Pool({
+console.log("auctionCount: ", auctionCount);
+const { Pool, Client } = require("pg");
+// const pool = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000
+// });
+const ahClient = new Client({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000
 });
 
-insertAHData = () => {
+ahClient.connect((err) => {
+  if (err) {
+    console.log("connection error: ", err);
+  } else {
+    console.log("client connected");
+    insertAHData(ahClient, (ahc) => {
+      ahc.end((err) => {
+        console.log("client has disconnected");
+        if (err) {
+          console.log("error occured during disconnect");
+        }
+      })
+    });
+  }
+});
 
+insertAHData = (ahc, callback) => {
   for (var i = 0; i < auctionCount; i++) {
-
     var insertStatement = `INSERT INTO ahRecord
         (groupId, auctionId, item, owner, ownerRealm,
         bid, buyout, quantity, timeLeft, rand, seed,
@@ -43,25 +65,17 @@ insertAHData = () => {
         ${ahData.auctions[i].petBreedId || null}, ${ahData.auctions[i].petLevel || null}, 
         ${ahData.auctions[i].petQualityId || null});
         `;
-
-    pool.connect((err, client, done) => {
+    ahc.query(insertStatement, (err, res) => {
       if (err) {
         console.log(err);
       } else {
-        client.query(insertStatement, (err, res) => {
-          if (err) {
-            console.log(err);
-          }
-          done();
-        });
       }
     });
-
+    if (i === auctionCount - 1) {
+      console.log("reached end");
+    }
   }
-
-  pool.end(() => {
-    console.log("pool has ended");
-  });
 }
 
-insertAHData();
+
+

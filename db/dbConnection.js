1b613d47693f386f7fb1073a82a31504b504896a
Jacques Begin <jacques.andre.begin@gmail.com>
2018-05-21 02:03:49 -0400

    Adds client query functionality to the pool connection.

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -12,14 +12,44 @@ const pool = new Pool({
 });
 
 
+
 module.exports = {
 
-  connect: () => {
-    
+  connectToPool: () => {
+    // const pool = new Pool({
+    //   user: process.env.PGUSER,
+    //   host: process.env.PGHOST,
+    //   database: process.env.PGDATABASE,
+    //   password: process.env.PGPASSWORD,
+    //   port: process.env.PGPORT,
+    //   max: 20,
+    //   idleTimeoutMillis: 30000,
+    //   connectionTimeoutMillis: 2000
+    // });
+  },
+
+  makeClientQuery: (newQuery) => {
+    return pool.connect((err, client, release) => {
+      if (err) {
+        return console.log("Error acquiring client: ", err.stack);
+      }
+      client.query(newQuery.queryText, newQuery.queryValues, (err, result) => {
+        release(true);
+        if (err) {
+          return console.log("Error executing query: ", err.stack);
+        }
+        console.log(result);
+        newQuery.queryCallback(err, result);
+      })
+    });
   }, 
 
   query: (text, params, callback) => {
     return pool.query(text, params, callback);
+  },
+
+  release: () => {
+
   },
 
   end: () => {

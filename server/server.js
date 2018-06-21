1ece8bafb7b463a829ed9db06dc402b2002efda9
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-21 17:56:58 -0400

    Adds sequelize db require statement to server.js, authenticates the db, adds db test call to ensure results return from db.

--- server/server.js
+++ server/server.js
@@ -1,31 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const auctionRoutes = require("../routes/auctionRoutes");
-const Sequelize = require("sequelize");
+const db = require("../db/dbConnection");
 
 const app = express();
 const PORT = 8883;
-require('dotenv').config();
-
-
-const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
-  host: process.env.PGHOST,
-  dialect: "postgres",
-  pool: {
-    max: 20,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log("Connected to the database.");
-    startAppServer();
-  })
-  .catch(err => {
-    console.error("Unable to connect to database: ", err);
-  })
 
 
 // attach routes to the express app
@@ -36,9 +15,23 @@ app.get("/", function(req, res) {
 });
 
 
+db.authenticate()
+  .then(() => {
+    console.log("Connected to the database.");
+    startAppServer();
+  })
+  .catch(err => {
+    console.error("Unable to connect to database: ", err);
+  })
+
+
 startAppServer = () => {
   var server = app.listen(process.env.PORT || PORT, () => {
     var port = server.address().port;
     console.log(`App server running on port ${port}`);
+    db.query('SELECT * FROM bossencounter')
+      .then((results) => {
+        console.log(results);
+      })
   });
 }

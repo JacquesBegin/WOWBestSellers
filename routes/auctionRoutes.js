6f5912350ae981eb6f58e5fef3d8f648dd083f7e
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-21 18:20:39 -0400

    Adds test db query to the auction GET route.

--- routes/auctionRoutes.js
+++ routes/auctionRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../db/dbConnection");
 
 router.get("/", function(req, res) {
-  res.send("Should return auctions");
+  db.query('SELECT * FROM bossencounter')
+      .then((results) => {
+        console.log(results);
+        res.send(results[0]);
+      })
+      .catch((err) => {
+        console.error("Error getting auctions. ERROR: ", err);
+      });
 });
 
 module.exports = router;

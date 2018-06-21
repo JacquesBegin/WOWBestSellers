66135838eaa728ed9172d78f5426513ee1a4f84c
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-21 13:53:04 -0400

    Adds sequelize instance and express route for getting all auctions.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,40 @@
 const express = require("express");
-
+const auctionRoutes = require("../routes/auctionRoutes");
+const Sequelize = require("sequelize");
 
 const app = express();
 const PORT = 8883;
+require('dotenv').config();
+
+
+const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+  host: process.env.PGHOST,
+  dialect: "postgres",
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connected to the database.");
+    startAppServer();
+  })
+  .catch(err => {
+    console.error("Unable to connect to database: ", err);
+  })
+
+
+// attach routes to the express app
+app.use("/auctions", auctionRoutes);
+
+app.get("/", function(req, res) {
+  res.send("Wow Best Sellers");
+});
+
 
 startAppServer = () => {
   var server = app.listen(process.env.PORT || PORT, () => {
@@ -10,5 +42,3 @@ startAppServer = () => {
     console.log(`App server running on port ${port}`);
   });
 }
-
-startAppServer();

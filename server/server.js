af6efcbe4d6e415f01b5ee82ed64f488689a752f
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-25 12:54:05 -0400

    Organizes code for app initialization.

--- server/server.js
+++ server/server.js
@@ -7,29 +7,18 @@ const auctionRoutes = require("./routes/auctionRoutes")(db);
 const app = express();
 const PORT = 8884;
 
-// Serve static files from the React build folder
+// Serve static files from the React build folder.
 app.use(express.static(path.join(__dirname, "/../client/build")));
 
+initializeRoutes = () => {
+  // Attach routes to the express app.
+  app.use("/auctions", auctionRoutes);
 
-// Attach routes to the express app
-app.use("/auctions", auctionRoutes);
-
-
-// Assign the catch all route to send the index.html page
-app.all("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-});
-
-
-db.authenticate()
-  .then(() => {
-    console.log("Connected to the database.");
-    startAppServer();
-  })
-  .catch(err => {
-    console.error("Unable to connect to database: ", err);
-  })
-
+  // Assign the catch all route to send the index.html page.
+  app.all("*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+  });  
+}
 
 startAppServer = () => {
   var server = app.listen(process.env.PORT || PORT, () => {
@@ -37,3 +26,14 @@ startAppServer = () => {
     console.log(`App server running on port ${port}`);
   });
 }
+
+// Ensure there is a connection to the database.
+db.authenticate()
+  .then(() => {
+    console.log("Connected to the database.");
+    initializeRoutes();
+    startAppServer();
+  })
+  .catch(err => {
+    console.error("Unable to connect to database: ", err);
+  })

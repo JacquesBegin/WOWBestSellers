84201b24d385bafe5d34c4e0b7d6a9dbf872d7c0
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-25 03:20:00 -0400

    Adds test code to react to connect front and back ends.

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    testString: {}
+  }
+
+  getTestStringFromAPI = () => {
+    fetch("/auctions/test")
+      .then(res => res.json())
+      .then(test => this.setState({testString: test}));
+  }
+
+  componentDidMount() {
+    this.getTestStringFromAPI();
+  }
+
   render() {
     return (
       <div>
         <h1>Wow Best Sellers</h1>
+        <div>{this.state.testString.test}</div>
       </div>
     );
   }

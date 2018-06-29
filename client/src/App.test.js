14ac1302b4b11146b075d285db197d4deefa31d8
Jacques Begin <jacques.andre.begin@gmail.com>
2018-06-28 22:16:46 -0400

    Incorporates enzyme testing code. Not fully working yet, issue with setting up adapter.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  // ReactDOM.render(<App />, div);
+  const wrapper = render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+  expect(wrapper.find('div')).to.have.length(1);
 });

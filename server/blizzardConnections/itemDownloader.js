673b44db24427974550fc4c9293a215f3301a009
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-02 19:00:47 -0400

    Adds functionality to get item data from blizzard, add item to database, and check for if item is found in db.

--- server/blizzardConnections/itemDownloader.js
+++ server/blizzardConnections/itemDownloader.js
@@ -5,10 +5,49 @@ const dbQueries = require("../db/queries");
 require('dotenv').config();
 
 
-const itemUrl = `https://us.api.battle.net/wow/item/18803?locale=en_US&apikey=78g9wcthpzzrahr6kjmmu3s79233th2u`;
 
 createItemsTable = () => {
   db.query(dbQueries.createItemsTable());
+}
+
+importItemDataFromBlizzard = (itemId) => {
+  let testItemId = 18803;
+  let itemUrl = `https://us.api.battle.net/wow/item/${itemId}?locale=en_US&apikey=78g9wcthpzzrahr6kjmmu3s79233th2u`;
+
+  request(
+    {
+      url: itemUrl,
+      json: true
+    }, function (err, res, data) {
+      if (!err & res.statusCode === 200) {
+        console.log(data);
+      } else {
+        console.log("Error retrieving item from Blizzard: ", err);
+      }
+    }
+  );
+
+}
+
+addItemToDB = (item) => {
+  let itemInfo = {};
+  itemInfo.itemid = item.itemId;
+  itemInfo.name = item.name;
+  itemInfo.description = item.description;
+  itemInfo.icon = item.icon;
+  itemInfo.stackable = item.stackable;
+  itemInfo.itembind = item.itemBind;
+  itemInfo.buyprice = item.buyPrice;
+  itemInfo.sellprice = item.sellPrice;
+  itemInfo.isauctionable = item.isAuctionable;
+
+  db.query(dbQueries.addItem(itemInfo))
+    .then(() => {
+      console.log(`Item ${itemInfo.itemid} added to database successfully`);
+    })
+    .catch((err) => {
+      console.log(`Error adding item ${itemInfo.itemid} to database: ${err}`);
+    })
 }
 
 retrieveItemDataFromDB = (itemId) => {
@@ -16,8 +55,10 @@ retrieveItemDataFromDB = (itemId) => {
     .then(db.query(dbQueries.getItem(itemId)))
     .then((response) => {
       console.log("response:", response);
-      if (response === null) {
+      if (!response[0].length) {
         console.log("null");
+      } else {
+        console.log("item found");
       }
     })
 }
@@ -26,3 +67,4 @@ retrieveItemDataFromDB = (itemId) => {
 
 createItemsTable();
 retrieveItemDataFromDB(1);
+importItemDataFromBlizzard(18803);

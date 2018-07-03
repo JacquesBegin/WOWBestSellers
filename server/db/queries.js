bc6a54163791750a62c111c5cbeac8ae387507d6
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-03 05:06:55 -0400

    Updates db query strings to use new column names.

--- server/db/queries.js
+++ server/db/queries.js
@@ -3,29 +3,29 @@
 module.exports = {
   createItemsTable: function() {
                     return (`CREATE TABLE IF NOT EXISTS items (
-                      itemid int NOT NULL,
+                      item_id int NOT NULL,
                       name varchar(100),
                       description varchar(450),
                       icon varchar(45),
                       stackable int,
-                      itembind int,
-                      buyprice int,
-                      sellprice int,
-                      isauctionable boolean
+                      item_bind int,
+                      buy_price int,
+                      sell_price int,
+                      is_auctionable boolean
                     );`);
                   },
   
   getItem: function(itemId) {
             return (`SELECT * FROM items
-              WHERE itemid = ${itemId};
+              WHERE item_id = ${itemId};
             `);
           },
 
   addItem: function(item) {
             return (`INSERT INTO items
-            (itemid, name, description, icon
-            stackable, itembind, buyprice,
-            sellprice, isauctionable) VALUES
+            (item_id, name, description, icon,
+            stackable, item_bind, buy_price,
+            sell_price, is_auctionable) VALUES
             (${item.itemid}, ${item.name}, ${item.description}, 
             ${item.icon}, ${item.stackable}, ${item.itembind},
             ${item.buyprice}, ${item.sellprice}, ${item.isauctionable}

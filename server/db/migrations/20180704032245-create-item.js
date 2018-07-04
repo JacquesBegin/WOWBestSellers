0bb2ab53722ac17fb1d63a57a4c4de0db99c778d
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-04 02:09:54 -0400

    Adds item migration file.

---
+++ server/db/migrations/20180704032245-create-item.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.TEXT,
+        required: true
+      },
+      item_id: {
+        type: Sequelize.INTEGER,
+        required: true
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      icon: {
+        type: Sequelize.TEXT
+      },
+      stackable: {
+        type: Sequelize.INTEGER
+      },
+      item_bind: {
+        type: Sequelize.INTEGER
+      },
+      buy_price: {
+        type: Sequelize.INTEGER
+      },
+      sell_price: {
+        type: Sequelize.INTEGER
+      },
+      is_auctionable: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, {
+      paranoid: true,
+      underscored: true
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Items');
+  }
+};

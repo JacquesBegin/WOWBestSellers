480651467dc4d792057a4835bf2163f0680956c6
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-05 03:41:59 -0400

    Creates functionality to import dump data, check if that data is in the db, and temporary addToDB functionality.

--- server/blizzardDataAccess/downloaders/ahDumpDownloader.js
+++ server/blizzardDataAccess/downloaders/ahDumpDownloader.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const auctionDownloader = require("./auctionDownloader");
 
 require('dotenv').config();
 
@@ -8,25 +9,74 @@ const url = `https://us.api.battle.net/wow/auction/data/thrall?locale=en_US&apik
 // url to Blizzard API for Alterac Mountains server ah information 
 // const url = `https://us.api.battle.net/wow/auction/data/alterac%20mountains?locale=en_US&apikey=${process.env.API_KEY}`;
 
-importAhDumpData = () => {
+importAhDumpData = (db) => {
   request(
     {
       url: url,
       json: true
-    }, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        console.log("firstbody:", body);
+    }, function (err, res, data) {
+      if (!err && res.statusCode === 200) {
+        console.log("firstbody:", data);
 
-        // check body.lastModified with previous request
-        // and only pull data if the content has been
-        // modified.
-        // if (body.lastModified !== XXXXXX.timeStamp)
+        let ahDump = {};
+        ahDump.url = data.files[0].url;
+        ahDump.lastModified = data.files[0].lastModified;
 
-        ahDownloadData.url = body.files[0].url;
-        ahDownloadData.lastModified = body.files[0].lastModified;
-        
+        checkIfDumpInDB(db, data.files[0].lastModified)
+          .then((result) => {
+            if (!result) {
+              addDumpToDB(db, ahDump)
+                .then((result) => {
+                  // Call auctionDownloader to import all
+                  // auctions from current dump.
+                })
+                .catch((err) => {
+                  console.log("Dump not added to database: ", err);
+                })
+            } else {
+              console.log("Dump already downloaded");
+            }
+          })
+      } else {
+        console.log("Error importing dump: ", err);
       }
     }
   );
 }
-  
+
+checkIfDumpInDB = (db, lastModified) => {
+  let promise = new Promise((resolve, reject) => {
+    db.dumps.findOne({
+      where: {
+        last_modified: lastModified
+      }
+    })
+    .then((dump) => {
+      if (dump) {
+        console.log("Dump in database.");
+        resolve(true);
+      } else {
+        console.log("Dump not in database.");
+        resolve(false);
+      }
+    })
+    .catch((err) => {
+      reject(err);
+    })
+  })
+  return promise;
+}
+
+addDumpToDB = (db, dump) => {
+  // Create a promise here and return it.
+  // The following .then should call the auction
+  // downloader and start the process of importing
+  // all auctions from the current dump.
+  let promise = new Promise((resolve, reject) => {
+    resolve();
+  })
+  return promise;
+}
+ 
+
+module.exports = importAhDumpData;

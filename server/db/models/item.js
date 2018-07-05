bdbfc2eb43d850ec0f3d49c132d39a84dec03d0e
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-05 02:29:48 -0400

    Updates item model with cleaner formatting.

--- server/db/models/item.js
+++ server/db/models/item.js
@@ -1,19 +1,44 @@
-'use strict';
+'use strict'
 
 module.exports = (sequelize, DataTypes) => {
-  var Item = sequelize.define('Item', {
-    name: DataTypes.TEXT,
-    item_id: DataTypes.INTEGER,
-    description: DataTypes.TEXT,
-    icon: DataTypes.TEXT,
-    stackable: DataTypes.INTEGER,
-    item_bind: DataTypes.INTEGER,
-    buy_price: DataTypes.INTEGER,
-    sell_price: DataTypes.INTEGER,
-    is_auctionable: DataTypes.BOOLEAN
-  });
-  Item.associate = function(models) {
-    // associations can be defined here
-  };
-  return Item;
+  const Item = sequelize.define('item', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    item_id: {
+      type: DataTypes.INTEGER,
+      required: true
+    },
+    name: {
+      type: DataTypes.TEXT('medium'),
+      required: true
+    },
+    description: {
+      type: DataTypes.TEXT('long')
+    },
+    icon: {
+      type: DataTypes.TEXT('medium')
+    },
+    stackable: {
+      type: DataTypes.INTEGER
+    },
+    item_bind: {
+      type: DataTypes.INTEGER
+    },
+    buy_price: {
+      type: DataTypes.INTEGER
+    },
+    sell_price: {
+      type: DataTypes.INTEGER
+    },
+    is_auctionable: {
+      type: DataTypes.BOOLEAN
+    }
+  }, {
+      paranoid: true,
+      underscored: true
+    });
+    return Item;
 };

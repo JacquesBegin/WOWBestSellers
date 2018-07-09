1f74981f944a932e47cf779d644958d7837e69e5
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-08 23:37:41 -0400

    Adds functionality to itemDownloader.js to query the auction table for item id's not in the items table and then downloads item info from blizzard.

--- server/blizzardDataAccess/downloaders/itemDownloader.js
+++ server/blizzardDataAccess/downloaders/itemDownloader.js
@@ -11,25 +11,28 @@ require('dotenv').config();
 // Requests item data from Blizzard and takes a callback 
 // to handle the returned data.
 importItemDataFromBlizzard = (db, itemId, callback) => {
-  let itemUrl = `https://us.api.battle.net/wow/item/${itemId}?locale=en_US&apikey=${process.env.API_KEY}`;
+  let promise = new Promise(function(resolve, reject) {
+    let itemUrl = `https://us.api.battle.net/wow/item/${itemId}?locale=en_US&apikey=${process.env.API_KEY}`;
 
-  request(
-    {
-      url: itemUrl,
-      json: true
-    }, function (err, res, data) {
-      if (!err & res.statusCode === 200) {
-        callback(db, data);
-      } else {
-        console.log("Error retrieving item from Blizzard: ", err);
+    request(
+      {
+        url: itemUrl,
+        json: true
+      }, function (err, res, data) {
+        if (!err & res.statusCode === 200) {
+          callback(db, data, resolve);
+        } else {
+          reject(`Error retrieving item from Blizzard: ERR: ${err}, RES: ${res}, DATA: ${data} `);
+        }
       }
-    }
-  );
+    );
+  })
+  return promise;
 }
 
 // Takes item data as a parameter and uses the 
 // item model to create a new row in the database
-addItemToDB = (db, item) => {
+addItemToDB = (db, item, success) => {
   db.items.create({
     item_id: item.id,
     name: item.name,
@@ -43,11 +46,35 @@ addItemToDB = (db, item) => {
   })
   .then((newItem) => {
     console.log(`Item ${newItem.name} (${newItem.item_id}) added to the database.`);
+    if (success) {
+      success();
+    }
   })
   .catch((err) => {
     console.log("Error during item INSERT: ", err);
   });
 }
+
+
+importAndAddMultipleItems = (db, items, count, itemsLength) => {
+  console.log("itemsLength: ", itemsLength);
+  if (count < itemsLength) {
+    console.log("items: ", items[count].item_id);
+    downloadNewItemToDb(db, items[count].item_id)
+      .then(() => {
+        count++;
+        setTimeout(() => {
+          process.nextTick(() => {importAndAddMultipleItems(db, items, count, itemsLength)});
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  } else {
+    console.log(`Finished importing all items.`);
+  }
+}
+
 
 // Checks database for an item by id, currently only returns
 // a boolean value to indicate if an item is found.
@@ -76,16 +103,59 @@ retrieveItemFromDB = (db, itemId) => {
 
 // Function to run itemDownloader functionality
 downloadNewItemToDb = (db, itemId) => {
-  retrieveItemFromDB(db, itemId)
-  .then((result) => {
-    if(!result) {
-      importItemDataFromBlizzard(db, itemId, addItemToDB);
-    }
-  })
-  .catch((err) => {
-    console.log("Error retrieving item from database: ", err);
+  let promise = new Promise(function(resolve, reject) {
+    retrieveItemFromDB(db, itemId)
+      .then((result) => {
+        if(!result) {
+          importItemDataFromBlizzard(db, itemId, addItemToDB)
+           .then(() => {
+            resolve();
+           })
+           .catch((err) => {
+             console.log(err);
+           });
+        }
+      })
+      .catch((err) => {
+        reject("Error retrieving item from database: ", err);
+      });
   });
+  return promise;
 }
 
 
-module.exports = downloadNewItemToDb;
+// Functionality to scan database auctions and add item 
+// information for items not already in the database.
+itemScanner = (db) => {
+  // Query auction table for items not in the item table.
+  // Only retrieve 1 instance of each item from auctions.
+  // Restrict results to be a limited quantity (ex. 1000 items)
+  // db.auctions.findAll({
+  //   attributes: ["item_id"],
+  //   where: {},
+  //   limit: 1000
+  // })
+  db.sequelize.query(`SELECT DISTINCT item_id FROM auctions WHERE 
+                    item_id NOT IN (SELECT item_id FROM 
+                    items) LIMIT 1000;`)
+    .then((items) => {
+      console.log("items after query: ", items[0]);
+      importAndAddMultipleItems(db, items[0], 0, items[0].length);
+    })
+    .catch((err) => {
+      console.log(`Error retrieving item list to import 
+      into database`);
+    });
+}
+
+removeDuplicateItemsFromDB = (db) => {
+  db.sequelize.query(`DELETE FROM items a USING items b
+                      WHERE a.id < b.id AND a.item_id = 
+                      b.item_id;`)
+}
+
+module.exports = {
+  downloadNewItemToDb: downloadNewItemToDb,
+  itemScanner: itemScanner
+}
+

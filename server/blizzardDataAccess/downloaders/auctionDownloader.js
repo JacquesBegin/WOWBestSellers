2400721c43a4f392460456310d26a04b75b92fa2
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-09 14:42:12 -0400

    Adds functionality to run the auction downloader using promises, removes comments, converts functions within the downloading process to be promises.

--- server/blizzardDataAccess/downloaders/auctionDownloader.js
+++ server/blizzardDataAccess/downloaders/auctionDownloader.js
@@ -6,113 +6,54 @@ require('dotenv').config();
 
 // Import all auctions for a specific auction house dump.
 importAHDataFromBlizzard = (db, dump) => {
+  let promise = new Promise((resolve, reject) => {
 
-  // var url = "http://auction-api-us.worldofwarcraft.com/auction-data/de55febb04096123926bca84d7b31f1c/auctions.json"
-
-  // Potentially add property to record how long the 
-  // download takes.
-
-  // request(
-  //   {
-  //     url: dump.url,
-  //     json: true
-  //   }, function (err, res, data) {
-  //     if (!err & res.statusCode === 200) {
-        
-  //       // var filename = `${__dirname}/../ahData/AHData_${Date.now()}.json`;
-
-  //       ahDownloadData.ahData = data;
-  //       ahDownloadData.filename = filename;
-
-  //       var currentAHData = JSON.stringify(ahDownloadData);
-        
-  //       console.log("passed loading");
-  //       fs.writeFile(filename, currentAHData, (err) => {
-  //         if (err) {
-  //           console.log(`err: ${err}`);
-  //         } else {
-  //           console.log(`File ${filename} saved`);
-  //         }
-  //       });
-  //     }
-  //   }
-  // );
+    // Possibly add property to record how long the 
+    // download takes.
 
   var filename = `${__dirname}/../ahData/AHData_${Date.now()}.json`;
 
   request.get(dump.url)
-  // request.get(url)
     .on('response', function(res) {
       console.log("res.statusCode: ", res.statusCode);
       console.log("res.headers: ", res.headers['content-type']);
     })
     .on('error', function(err) {
       console.log("Error requesting auction house data: ", err);
+      reject(err);
     })
     .on('end', function() {
       console.log("Auction file created.");
-      // Once the stream has ended, add auctions to DB
-      addAuctionsToDB(db, dump.id, filename);
+      // Once the stream has ended, resolve with auction
+      // dump data.
+      resolve({
+        db: db,
+        dumpId: dump.id,
+        filename: filename
+      });
+      // addAuctionsToDB(db, dump.id, filename);
     })
     .pipe(fs.createWriteStream(filename))
+  });
+  return promise;
 }
 
-// Add auctions to the database from a json file
-addAuctionsToDB = async (db, dumpId, file) => {
+// Extract data to use for inserting auctions and add 
+// auctions to the database from a json file.
+addAuctionsToDB = (db, dumpId, file) => {
+  let promise = new Promise((resolve, reject) => {
+    const jsonData = require(file);
+    const auctions = jsonData.auctions;
+    const auctionCount = 0;
+    const auctionLength = auctions.length;
 
-  // const tempDB = require("../../db/config/index.js");
-
-  const jsonData = require(file);
-  // const jsonData = require("../ahData/AHData_1530834639900.json");
-  console.log("jsonData.realms: ", jsonData.realms);
-  const auctions = jsonData.auctions;
-  const auctionCount = 0;
-  const auctionLength = auctions.length;
-
-  console.log("auction: ", auctions[0]);
-
-  // for(let auc of auctions) {
-  //   await insertAuction(tempDB, dumpId, auc);
-  // }
-
-  // auctions.map((auc) => {
-  //   insertAuction(tempDB, dumpId, auc);
-  //   return null;
-  // })
-
-  insertAuction(db, dumpId, auctions, auctionCount, auctionLength);
-
-  // let auction = auctions[0];
-  // auctions.forEach(function(auction) {
-  // for(let auction in auctions) {
-    // tempDB.auctions.create({
-  //   db.auctions.create({
-  //     dump_id: dumpId,
-  //     auction_id: auction.auc,
-  //     item_id: auction.item,
-  //     owner: auction.owner,
-  //     owner_realm: auction.ownerRealm,
-  //     bid: auction.bid,
-  //     buyout: auction.buyout,
-  //     quantity: auction.quantity,
-  //     time_left: auction.timeLeft,
-  //     rand: auction.rand,
-  //     seed: auction.seed,
-  //     context: auction.context,
-  //     pet_species_id: auction.petSpeciesId || null,
-  //     pet_breed_id: auction.petBreedId || null,
-  //     pet_level: auction.petLevel || null,
-  //     pet_quality_id: auction.petQualityId || null
-  //   })
-  //   .then((auction) => {
-  //   })
-  //   .catch((err) => {
-  //     console.log("Error during auction INSERT: ", err);
-  //   });
-  // });
+    insertAuctions(db, dumpId, auctions, auctionCount, 
+      auctionLength, {resolve: resolve, reject: reject});
+  });
+  return promise;
 }
 
-insertAuction = (db, dumpId, auctions, auctionCount, auctionLength) => {
+insertAuctions = (db, dumpId, auctions, auctionCount, auctionLength, resolution) => {
   if (auctionCount < auctionLength) {
     let auction = auctions[auctionCount];
     // let promise = new Promise((resolve, reject) => {
@@ -135,30 +76,50 @@ insertAuction = (db, dumpId, auctions, auctionCount, auctionLength) => {
         pet_quality_id: auction.petQualityId || null
       })
       .then((result) => {
-        console.log("INSERT successful: ", auctionCount);
         auctionCount++;
         // Using process.nextTick() causes the next 
-        // insertAuction call to be called after the 
+        // insertAuctions call to be called after the 
         // current "tick"/loop in the node event loop.
         // Using process.nextTick() locks the application
         // causing any I/O process to wait until after 
         // the last process.nextTick() is finished.
-        process.nextTick(() => {insertAuction(db, dumpId, auctions, auctionCount, auctionLength)});
+        process.nextTick(() => {insertAuctions(db, dumpId, auctions, auctionCount, auctionLength)});
       })
       .catch((err) => {
         console.log("Error during auction INSERT: ", err);
+        resolution.reject(err);
       });
     // });
     // return promise;
-    
   } else {
     console.log(`Auctions from dump ${dumpId} INSERTed`);
+    resolution.resolve(true);
   }
 }
 
+runAuctionDownloader = (db, dump) => {
+  let promise = new Promise((resolve, reject) => {
+    importAHDataFromBlizzard(db, dump)
+    .then((result) => {
+      addAuctionsToDB(result.db, result.dumpId, result.filename);
+    })
+    .then((result) => {
+      console.log(`Done adding auctions: ###${result}###`);
+      resolve(true);
+    })
+    .catch((err) => {
+      console.log("Error running auction downloader: ", err);
+      reject(err);
+    })
+  });
+  return promise;
+}
 
-module.exports = importAHDataFromBlizzard;
 
+module.exports = {
+  importAHDataFromBlizzard: importAHDataFromBlizzard,
+  runAuctionDownloader: runAuctionDownloader
+}
 // addAuctionsToDB("db", 1111111, "fakeFile");
 
 

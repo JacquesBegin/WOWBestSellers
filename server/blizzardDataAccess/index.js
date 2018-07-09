4b53c297e81c9aa39de9025b56aa9f640fb232b2
Jacques Begin <jacques.andre.begin@gmail.com>
2018-07-09 14:37:54 -0400

    Creates a function for running the full data downloader.

--- server/blizzardDataAccess/index.js
+++ server/blizzardDataAccess/index.js
@@ -4,5 +4,19 @@ const ahDumpDownloader = require("./downloaders/ahDumpDownloader");
 
 module.exports = {
   downloadDump: ahDumpDownloader,
-  importNewItems: itemDownloader.itemScanner
+  importNewItems: itemDownloader.itemScanner,
+  downloadAuctions: auctionDownloader,
+  runDataImportAutomation: function(db) {
+                            ahDumpDownloader(db)
+                              .then((dump) => {
+                                return auctionDownloader.runAuctionDownloader(db, dump)
+                              })
+                              .then((result) => {
+                                console.log("In index, running automation, ready to start item download.");
+                                itemDownloader.itemScanner(db);
+                              })
+                              .catch((err) => {
+                                console.log("Error during automation: ", err);
+                              });
+                          }
 }
